f13623aab49618745c87ee9920723c59143ab5f5
Jasprit Grewal <153491291+sinferna@users.noreply.github.com>
2025-09-15 18:20:25 -0600

    ready for submission
    - created StudentInfo component
    - rendered StudentInfo in week-2 page
    - edited root page code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Jasprit Grewal (SID: 000953380)</p>
+      GitHub repo: {" "}
+      <a href="https://github.com/sinferna/cprg306-assignments" target="_blank">https://github.com/sinferna/cprg306-assignments</a>
+    </div>
+  );
+}
